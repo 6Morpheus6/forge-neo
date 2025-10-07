b9abaa7d45238a57f1cd61ab2975dbb71835dba6
6Morpheus6 <wanton-decay@gmx.net>
2025-10-07 12:55:54 +0200

    nunchaku for torch 2.7.0

--- torch.js
+++ torch.js
@@ -11,6 +11,7 @@ module.exports = {
           "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps",
           "uv pip install https://github.com/6Morpheus6/deepspeed-windows-wheels/releases/download/v0.17.5/deepspeed-0.17.5+e1560d84-2.7torch+cu128-cp310-cp310-win_amd64.whl",
           "uv pip install -U bitsandbytes --force-reinstall --no-deps",
+          "uv pip install https://huggingface.co/nunchaku-tech/nunchaku/resolve/main/nunchaku-1.0.1%2Btorch2.7-cp310-cp310-win_amd64.whl",
           "{{args && args.triton ? 'uv pip install triton-windows==3.3.1.post19' : ''}}",
           "{{args && args.sageattention ? 'uv pip install https://github.com/woct0rdho/SageAttention/releases/download/v2.1.1-windows/sageattention-2.1.1+cu128torch2.7.0-cp310-cp310-win_amd64.whl' : ''}}",
         ]
@@ -27,6 +28,7 @@ module.exports = {
         "message": [
           "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall --no-deps",
           "uv pip install -U bitsandbytes --force-reinstall --no-deps",
+          "uv pip install https://huggingface.co/nunchaku-tech/nunchaku/resolve/main/nunchaku-1.0.1%2Btorch2.7-cp310-cp310-linux_x86_64.whl?",
           "{{args && args.sageattention ? 'uv pip install https://huggingface.co/MonsterMMORPG/SECourses_Premium_Flash_Attention/resolve/main/sageattention-2.1.1%2Bcu128torch2.7.1-cp310-cp310-win_amd64.whl?download=true' : ''}}",
         ]
       },
